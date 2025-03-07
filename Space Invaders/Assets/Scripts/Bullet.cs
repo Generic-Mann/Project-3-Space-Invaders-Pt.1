2f1436bdc2de4637588b7e0974d403d6d6891507
Michael Garcia <157657140+Generic-Mann@users.noreply.github.com>
2025-03-06 21:22:53 -0800

    Finished Space Invaders Pt.1

---
+++ Space Invaders/Assets/Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] //technique for making sure there isn't a null reference during runtime if you are going to use get component
+public class Bullet : MonoBehaviour
+{
+  private Rigidbody2D myRigidbody2D;
+
+  public delegate void EnemyDied(int points);
+  public static event EnemyDied OnEnemyDied;
+
+  public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+      myRigidbody2D = GetComponent<Rigidbody2D>();
+      Fire();
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+
+      if (collision.gameObject.CompareTag("Octo"))
+      {
+        OnEnemyDied?.Invoke(10);
+      }
+      else if (collision.gameObject.CompareTag("Crab"))
+      {
+        OnEnemyDied?.Invoke(20);
+      }
+      else if (collision.gameObject.CompareTag("Squid"))
+      {
+        OnEnemyDied?.Invoke(30);
+      }
+      else if (collision.gameObject.CompareTag("Ufo"))
+      {
+        int ufoPoints = Random.Range(1, 7) * 50;
+        OnEnemyDied?.Invoke(ufoPoints);
+      }
+
+      Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    private void Fire()
+    {
+      myRigidbody2D.linearVelocity = Vector2.up * speed; 
+    }
+}
---
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bulletPreFab;
+
+  public Transform shottingOffset;
+
+  Animator playerAnimator;
+
+  private int score = 0;
+  private int hiscore = 0;
+  public float speed = 5f;
+
+  public TextMeshProUGUI scoreText;
+  public TextMeshProUGUI hiscoreText;
+
+
+  void Start()
+  {
+    Bullet.OnEnemyDied += EnemyOnOnEnemyDied;
+    playerAnimator = GetComponent<Animator>();
+    hiscore = PlayerPrefs.GetInt("HighScore", 0);
+    score = 0;
+    hiscoreText.text = $"HI-SCORE\n{hiscore:D4}";
+  }
+
+  void OnDestroy()
+  {
+    Bullet.OnEnemyDied -= EnemyOnOnEnemyDied;
+  }
+
+  void EnemyOnOnEnemyDied(int points)
+  {
+
+    Debug.Log($"Enemy killed, you scored: {points}");
+
+    score += points;
+
+    if (score > hiscore)
+    {
+        hiscore = score;
+
+        PlayerPrefs.SetInt("HighScore", hiscore);
+        PlayerPrefs.Save();
+
+        Debug.Log($"New High Score: {hiscore}!");
+    }
+    scoreCounts();
+  }
+
+    void scoreCounts()
+    {
+      scoreText.text = $"SCORE\n{score:D4}";
+      hiscoreText.text = $"HI-SCORE\n{hiscore:D4}";
+    }
+
+  
+    // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      playerAnimator.SetTrigger("Shoot");
+
+      GameObject shot = Instantiate(bulletPreFab, shottingOffset.position, Quaternion.identity);
+
+      Destroy(shot, 3f);
+
+    }
+
+    float moveDirection = 0f;
+
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      moveDirection = -1f;
+    }
+    else if (Input.GetKey(KeyCode.RightArrow))
+    {
+      moveDirection = 1f;
+    }
+    transform.position += new Vector3(moveDirection * speed * Time.deltaTime, 0, 0);
+  }
+}
+
